79356d35b66ca4c25f68dc4d5870b76b45e09bf5
joshua richards <jermain119@gmail.com>
2019-11-12 20:36:56 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -18,5 +18,5 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
-SELECT * FROM students WHERE name = "Maru   "SELECT AVG(gpa) FROM grade;"
+"SELECT * FROM students WHERE grade = 9" #SELECT AVG(gpa) FROM grade;"
 end
